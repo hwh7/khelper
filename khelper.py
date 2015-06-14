f7e0c86a6c1ef00fae43a44a46e8d703ea6b3bb6
Hyungwon Hwang <hyungwon.hwang7@gmail.com>
2015-06-14 10:30:50 +0900

    test_send-email: Add the feature for testing sending patch files
    This feature is used to send patch files just to me for testing.
    
    Usage:
    ./khelper test_sendemail [PATCH_FILES]
    
    Signed-off-by: Hyungwon Hwang <hyungwon.hwang7@gmail.com>

--- khelper.py
+++ khelper.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python
 
 from sys import argv
+from subprocess import Popen, PIPE
 
-def print_usage():
-	print "print_usage"
+def print_usage(argv):
+	print 'Khelper for kernel development'
+	print 'Usage: %s [COMMAND] [ARGUMENTS]' % argv[0]
+	print 'test_sendemail [PATCH_FILES]: send patch files only to me'
+
+def get_usermail():
+	p = Popen(['git', 'config', 'user.email'], stdout=PIPE)
+	output = p.communicate()
+	return output
+
+def test_sendemail(argv):
+	email = get_usermail()[0][0:-1]
+
+	for i in range(2, len(argv)):
+		Popen(['git', 'send-email', '--suppress-cc', 'all', '--to', email,  argv[i]]).communicate()
 
 funcs = {
+	'test_send-email': test_sendemail,
 }
 
 def main():
 	if len(argv) < 2:
-		print_usage()
+		print_usage(argv)
 		exit(0)
 
 	funcs[argv[1]](argv)
