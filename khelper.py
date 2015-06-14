ddf4f8f82560f9c9213aab6423c8e148bb74d2f5
Hyungwon Hwang <hyungwon.hwang7@gmail.com>
2015-06-14 11:02:48 +0900

    mapply: Add the feature for applying multiple patch files
    This feature is used for applying multiple patch files at once.
    
    Usage:
    ./khelper mapply [STARTING PATCH_FILE_NUMBER] [END PATCH_FILE_NUMBER]
    
    Signed-off-by: Hyungwon Hwang <hyungwon.hwang7@gmail.com>

--- khelper.py
+++ khelper.py
@@ -2,11 +2,13 @@
 
 from sys import argv
 from subprocess import Popen, PIPE
+from os import listdir
 
 def print_usage(argv):
 	print 'Khelper for kernel development'
 	print 'Usage: %s [COMMAND] [ARGUMENTS]' % argv[0]
 	print 'test_sendemail [PATCH_FILES]: send patch files only to me'
+	print 'mapply [STARTING PATCH_FILE_NUMBER] [END PATCH_FILE_NUMBER]: apply multiple patch files which are prefixed by the number'
 
 def get_usermail():
 	p = Popen(['git', 'config', 'user.email'], stdout=PIPE)
@@ -19,8 +21,24 @@ def test_sendemail(argv):
 	for i in range(2, len(argv)):
 		Popen(['git', 'send-email', '--suppress-cc', 'all', '--to', email,  argv[i]]).communicate()
 
+def mapply(argv):
+	files = listdir('.')
+
+	i = int(argv[2])
+	end = int(argv[3])
+	for file in files:
+		if file.endswith('patch'):
+			if file.startswith('%.04d' % i):
+				p = Popen(['git', 'am', file])
+				output = p.communicate()
+				i += 1
+
+		if i > end:
+			break
+
 funcs = {
 	'test_send-email': test_sendemail,
+	'mapply': mapply,
 }
 
 def main():
